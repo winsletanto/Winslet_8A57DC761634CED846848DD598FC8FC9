e5442cf6408880591cbec6da74dba2b450c15570
winsletanto <144220046+winsletanto@users.noreply.github.com>
2023-09-06 15:03:49 +0530

    Update main.py

--- Challenge Unit 1/main.py
+++ Challenge Unit 1/main.py
@@ -1,1 +1,11 @@
-#This is main.py
+def fact(n):
+  if (n == 0):
+    return 1
+  else:
+    return n * fact(n - 1)
+
+def main():
+  inp = int(input("Enter a number to calculate factorial : "))
+  out = fact(inp)
+  print("Factorial of the given number : ", out)
+main()
